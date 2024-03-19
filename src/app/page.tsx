2ab075f0a72fff00f7723ddee0e10a2a52b2f963
Swayam-Shree <61933177+Swayam-Shree@users.noreply.github.com>
2024-03-19 06:52:38 +0530

    feat: add material ui config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
 export default function Home() {
 	return (
-		<div>Hellow</div>
+		<div className="flex flex-col">
+			<Typography variant="h1">Hello, world!</Typography>
+			<TextField label="Enter your name" />
+			<Button variant="contained">Submit</Button>
+		</div>
 	);
 }
