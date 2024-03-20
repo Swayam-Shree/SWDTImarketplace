f73dd057dbe12a089bd9ce27d2733f5d105bf0d8
Swayam-Shree <61933177+Swayam-Shree@users.noreply.github.com>
2024-03-20 20:56:18 +0530

    feat: mongo db connection config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
+import './mongo';
+
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
-import "./globals.css";
+import './globals.css';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
