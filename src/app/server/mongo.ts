1bb73ff821c9e0ab5e4fd156257e1f7bb2baec18
Swayam-Shree <61933177+Swayam-Shree@users.noreply.github.com>
2024-03-22 15:44:04 +0530

    feat: add create auction backend, auctions can be created with all important details except images and videos

--- src/app/server/mongo.ts
+++ src/app/server/mongo.ts
@@ -6,21 +6,13 @@ const uri = MONGO_URI;
 const mongoClient = new MongoClient(uri);
 
 let db: Db;
-let auctionsCollection: Collection;
+export let auctionsCollection: Collection;
 
 async function setup() {
 	await mongoClient.connect();
 
 	db = mongoClient.db('SWDTI');
 	auctionsCollection = db.collection('auctions');
-	
-	// await c.insertOne({
-	// 	name: 'test',
-	// 	description: 'test desc',
-	// 	basePrice: 100,
-	// 	duration: 100,
-	// 	createdAt: new Date()
-	// });
 }
 
 setup();
--- src/pages/api/socket.ts
+++ src/pages/api/socket.ts
@@ -1,6 +1,8 @@
 import { Server } from 'socket.io';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
+import { auctionsCollection } from '@/app/server/mongo';
+
 const SocketHandler = (req: NextApiRequest, res: NextApiResponse) => {
 	// @ts-expect-error
 	if (!res.socket.server.io) {
@@ -11,6 +13,10 @@ const SocketHandler = (req: NextApiRequest, res: NextApiResponse) => {
 
 		io.on('connection', (socket) => {
 			console.log('socket connected');
+
+			socket.on('createAuction', (data) => {
+				auctionsCollection.insertOne(data);
+			});
 
 			socket.on('disconnect', () => {
 				console.log('socket disconnected');
